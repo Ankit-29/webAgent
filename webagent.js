5969b9f6f9244c1b0597b1ad39e4bb16fc65e5c0
Ankit-29 <iamankitkumar29@gmail.com>
2020-08-13 22:20:32 +0530

    Title Fix

--- webagent.js
+++ webagent.js
@@ -9,7 +9,7 @@ getBrowser = () => {
     browser = (/chromium/i).test(userAgent) ? 'Chromium' : browser;
     browser = (/firefox|fxios/i).test(userAgent) && !(/seamonkey/i).test(userAgent) ? 'Firefox' : browser;
     browser = (/; msie|trident/i).test(userAgent) && !(/ucbrowser/i).test(userAgent) ? 'IE' : browser;
-    browser = (/chrome|crios/i).test(userAgent) && !(/opr|opera|chromium|edg|ucbrowser|googlebot/i).test(userAgent) ? 'Chrome' : browser;;
+    browser = (/chrome|crios/i).test(userAgent) && !(/opr|opera|chromium|edg|ucbrowser|googlebot/i).test(userAgent) ? 'Chrome' : browser;
     browser = (/safari/i).test(userAgent) && !(/chromium|edg|ucbrowser|chrome|crios|opr|opera|fxios|firefox/i).test(userAgent) ? 'Safari' : browser;
     browser = (/opr|opera/i).test(userAgent) ? 'Opera' : browser;
 
