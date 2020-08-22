b84d0630b9650f4a50d05ec24956722ec87179af
Ankit-29 <iamankitkumar29@gmail.com>
2020-08-22 20:51:56 +0530

    feat: DeviceType

--- webagent.js
+++ webagent.js
@@ -15,15 +15,15 @@ getBrowser = () => {
 
     // detect browser version
     switch (browser) {
-        case 'UCBrowser': return `${browser}/${browserVersion(userAgent,/(ucbrowser)\/([\d\.]+)/i)}`;
-        case 'Edge': return `${browser}/${browserVersion(userAgent,/(edge|edga|edgios|edg)\/([\d\.]+)/i)}`;
-        case 'GoogleBot': return `${browser}/${browserVersion(userAgent,/(googlebot)\/([\d\.]+)/i)}`;
-        case 'Chromium': return `${browser}/${browserVersion(userAgent,/(chromium)\/([\d\.]+)/i)}`;
-        case 'Firefox': return `${browser}/${browserVersion(userAgent,/(firefox|fxios)\/([\d\.]+)/i)}`;
-        case 'Chrome': return `${browser}/${browserVersion(userAgent,/(chrome|crios)\/([\d\.]+)/i)}`;
-        case 'Safari': return `${browser}/${browserVersion(userAgent,/(safari)\/([\d\.]+)/i)}`;
-        case 'Opera': return `${browser}/${browserVersion(userAgent,/(opera|opr)\/([\d\.]+)/i)}`;
-        case 'IE': const version = browserVersion(userAgent,/(trident)\/([\d\.]+)/i);
+        case 'UCBrowser': return `${browser}/${browserVersion(userAgent, /(ucbrowser)\/([\d\.]+)/i)}`;
+        case 'Edge': return `${browser}/${browserVersion(userAgent, /(edge|edga|edgios|edg)\/([\d\.]+)/i)}`;
+        case 'GoogleBot': return `${browser}/${browserVersion(userAgent, /(googlebot)\/([\d\.]+)/i)}`;
+        case 'Chromium': return `${browser}/${browserVersion(userAgent, /(chromium)\/([\d\.]+)/i)}`;
+        case 'Firefox': return `${browser}/${browserVersion(userAgent, /(firefox|fxios)\/([\d\.]+)/i)}`;
+        case 'Chrome': return `${browser}/${browserVersion(userAgent, /(chrome|crios)\/([\d\.]+)/i)}`;
+        case 'Safari': return `${browser}/${browserVersion(userAgent, /(safari)\/([\d\.]+)/i)}`;
+        case 'Opera': return `${browser}/${browserVersion(userAgent, /(opera|opr)\/([\d\.]+)/i)}`;
+        case 'IE': const version = browserVersion(userAgent, /(trident)\/([\d\.]+)/i);
             // IE version is mapped using trident version 
             // IE/8.0 = Trident/4.0, IE/9.0 = Trident/5.0
             return version ? `${browser}/${parseFloat(version) + 4.0}` : `${browser}/7.0`;
@@ -31,10 +31,14 @@ getBrowser = () => {
     }
 }
 
-browserVersion = (userAgent,regex) => {
+browserVersion = (userAgent, regex) => {
     return userAgent.match(regex) ? userAgent.match(regex)[2] : null;
 }
 
+getDeviceType = () => {
+    const userAgent = navigator.userAgent;
+    return /mobi/i.test(userAgent) ? 'Mobile' : 'Desktop' ;
+}
 
 
 
